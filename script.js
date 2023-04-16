28f8c9de741d88c0a2519da2fe331f6f8e3b49f0
surajkadm <68494225+surajkadm@users.noreply.github.com>
2023-04-17 01:36:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+touristSpots.map((element)=>{
+	let words=element.split(" ");	
+	let reducedList=words.reduce((prev,current,index)=>{
+		if(current!="The"&&current!="an"&&current!="a"){
+			return prev+" "+current;
+		}
+		return prev;
+	},"")
+	return reducedList;
+})
+console.log(touristSpots)
